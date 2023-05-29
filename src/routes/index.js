2ce55c27652df5a2abef96409419d16257ce7e5b
mppatel43 <mppatel43@myseneca.ca>
2023-05-28 21:09:24 -0400

    Add routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -15,7 +15,7 @@ const { authenticate } = require('../authentication');
 /**
  * Expose all of our API routes on /v1/* to include an API version.
  */
-router.use(`/v1`, require('./api'));
+//router.use(`/v1`, require('./api'));
 
 /**
  * Expose all of our API routes on /v1/* to include an API version.
