f7f4703799028550295b1f060c3d70e658462a1a
mppatel43 <mppatel43@myseneca.ca>
2023-06-12 19:51:29 -0400

    changing srcc/app.js lab4 correction

--- src/app.js
+++ src/app.js
@@ -28,9 +28,6 @@ app.use(compression());
 passport.use(authenticate.strategy());
 app.use(passport.initialize());
 
-// Define our routes
-app.use('/', require('./routes'));
-
 // Use logging middleware
 app.use(pino);
 
